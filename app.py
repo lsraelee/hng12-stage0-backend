fda7410a2d58542dc2af4d7ee3f53354f49c9dec
D'User <dwriter@Israels-MacBook-Pro.local>
2025-01-31 01:42:56 +0100

    Another commit based on corrections

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
 from flask_cors import CORS
 from datetime import datetime
-from collections import OrderedDict
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -9,13 +9,15 @@ CORS(app)
 
 @app.route("/", methods=["GET"])
 def home():
-    response = OrderedDict([
-        ("email", "ayomikunadewemimo@gmail.com"),
-        ("current_datetime", datetime.utcnow().isoformat()),
-        ("github_url", "https://github.com/lsraelee/hng12-stage0-backend.git")
-    ])
-    
-    return jsonify(response), 200
+    response_data = {
+        "email": "ayomikunadewemimo@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat(),
+        "github_url": "https://github.com/lsraelee/hng12-stage0-backend.git"
+    }
+    # Serialize JSON while preserving order
+    response_json = json.dumps(response_data, indent=4)
+    # Return response with status 200
+    return Response(response_json, status=200, content_type="application/json")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
